d3f5fa4f2da11364c37139ee25f8da4b7f3fe781
Tatyanaver <91628615+Tatyanaver@users.noreply.github.com>
2022-02-16 01:20:30 +0200

    Delete car.service.js

--- src/services/car.service.js
+++
@@ -1,8 +0,0 @@
-import {axiosService} from "./axios.service";
-import {urls} from "../config";
-
-export const carService = {
-    getAll: ()=> axiosService.get(urls.cars).then(value=>value.data),
-    createCar:(car)=> axiosService.post(urls.cars,car).then(value => value.data),
-    deleteById: (id)=> axiosService.delete(`${urls.cars}/${id}`),
-}
