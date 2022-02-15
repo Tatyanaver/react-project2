15f4853590eabeddb7a881fb1362973ba5130fa1
Tatyanaver <tpetrunya@ukr.net>
2022-02-16 00:55:35 +0200

    react pro

--- src/store/store.config.js
+++ src/store/store.config.js
@@ -1,11 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import movieReducer from "./movie.slice";
 
+
+
 const store = configureStore({
     reducer:{
         movieReducer: movieReducer,
         // genreReducer: genreReducer,
     }
 })
-
 export default store
