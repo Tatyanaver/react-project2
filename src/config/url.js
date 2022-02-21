abe7e1b8d19689e8dea27b85a562c53b60a61224
Tatyanaver <91628615+Tatyanaver@users.noreply.github.com>
2022-02-21 16:37:19 +0200

    Add files via upload

--- src/config/url.js
+++ src/config/url.js
@@ -1,8 +1,12 @@
+
 const baseURL ='https://api.themoviedb.org/3'
+
+// export const page = 1
 
 export const urls = {
     movies: '/discover/movie',
-    genres: '/genre/movie/list'
+    genres: '/genre/movie/list',
+    movie: '/movie',
 }
 
 export const apiKey = '1c372dcd45ea24a41cc1da16374fd486'
